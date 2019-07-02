f764412ecbe7a7e99a6a83aa54ad72fc6a611aac
Vitaly Kvitkovskiy <integralik@gmail.com>
2019-07-02 12:59:23 +0300

    changed sw to app.js

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/service_worker/sw.js', { scope: '/service_worker/' }).then(function(reg) {
-    // регистрация сработала
-    console.log('Registration succeeded. Scope is ' + reg.scope);
-  }).catch(function(error) {
-    // регистрация прошла неудачно
-    console.log('Registration failed with ' + error);
-  });
-};
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        '/service_worker/index.html'
+      ]);
+    })
+  );
+});
